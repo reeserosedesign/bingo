374fda0cb4da5b086981f9e778efcfceac6c3aed
Reese Rose <reeseroseux@gmail.com>
2023-03-16 23:44:27 -0400

    Added a New Game!

--- src/index.js
+++ src/index.js
@@ -22,31 +22,31 @@ function Tile({ id, children, onToggle, isSet }) {
 }
 
 const redflag = [
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
-  "-",
+  "Touches Dirty Feet",
+  "Conservative",
+  "Alcoholic",
+  "Religious",
+  "Bad Grammar",
+  "Halitosis",
+  "Kangaroo Feet",
+  "Musician",
+  "Film Student",
+  "Dead Animal in Photos",
+  "Trauma Bonded",
+  "Hates Rich People",
+  "Works in Finance",
+  "Doesn't Vote",
+  `"Just Ask"`,
+  "Boring Texter",
+  "Capricorn",
+  "Straight",
+  "Never Wrong",
+  "Takes Up Too Much Space",
+  "Likes Younger Girls",
+  "Hates Winnie the Pooh",
+  "Only Likes Anime",
+  "Hates Cartoons",
+  "Joe",
 ];
 
 const bbb = [
@@ -77,7 +77,12 @@ const bbb = [
   "In Florida",
 ];
 
-const data = shuffle(bbb).reduce(
+const placeData = shuffle(bbb).reduce(
+  (data, value, index) => ({ ...data, [index]: value }),
+  {}
+);
+
+const peopleData = shuffle(redflag).reduce(
   (data, value, index) => ({ ...data, [index]: value }),
   {}
 );
@@ -110,6 +115,26 @@ function App() {
       };
     });
 
+  const [isPlaceShown, setIsPlaceShown] = useState(true);
+  const [isPeopleShown, setIsPeopleShown] = useState(false);
+
+  const [isPlaceActive, setIsPlaceActive] = useState(true);
+  const [isPeopleActive, setIsPeopleActive] = useState(false);
+
+  const handlePlaceClick = (event) => {
+    setIsPlaceShown(true);
+    setIsPeopleShown(false);
+    setIsPlaceActive(true);
+    setIsPeopleActive(false);
+  };
+
+  const handlePeopleClick = (event) => {
+    setIsPeopleShown(true);
+    setIsPlaceShown(false);
+    setIsPeopleActive(true);
+    setIsPlaceActive(false);
+  };
+
   return (
     <div className="App">
       <h1>Bingo from Hell</h1>
@@ -119,27 +144,56 @@ function App() {
         day of this miserable life!
       </p>
       <div className="buttons">
-      <button>Places</button>
-      <button className="inactive">People</button></div>
-      <div className="wrapper">
-        {Object.keys(data).map((id) => (
-          <Tile
-            key={id}
-            id={id}
-            isSet={!!state.checked[id]}
-            onToggle={() => toggle(id)}
-          >
-            {data[id]}
-          </Tile>
-        ))}
+        <button
+          onClick={handlePlaceClick}
+          className={isPlaceActive ? "active" : ""}
+        >
+          Places
+        </button>
+
+        <button
+          onClick={handlePeopleClick}
+          className={isPeopleActive ? "active" : ""}
+        >
+          People
+        </button>
+        {isPlaceShown && (
+          <div className="wrapper">
+            {Object.keys(placeData).map((id) => (
+              <Tile
+                key={id}
+                id={id}
+                isSet={!!state.checked[id]}
+                onToggle={() => toggle(id)}
+              >
+                {placeData[id]}
+              </Tile>
+            ))}
+          </div>
+        )}
+        {isPeopleShown && (
+          <div className="wrapper">
+            {Object.keys(peopleData).map((id) => (
+              <Tile
+                key={id}
+                id={id}
+                isSet={!!state.checked[id]}
+                onToggle={() => toggle(id)}
+              >
+                {peopleData[id]}
+              </Tile>
+            ))}
+          </div>
+        )}
+
+        <footer>
+          <a href="https://www.reeserosedesign.com/" target="blank">
+            Reese Rose Design
+          </a>{" "}
+          © 2023
+        </footer>
+        {state.won ? <Confetti /> : null}
       </div>
-      <footer>
-        <a href="https://www.reeserosedesign.com/" target="blank">
-          Reese Rose Design
-        </a>{" "}
-        © 2023
-      </footer>
-      {state.won ? <Confetti /> : null}
     </div>
   );
 }
